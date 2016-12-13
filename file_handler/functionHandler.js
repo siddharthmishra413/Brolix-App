71d3307354f018de0c1128677c987c3c17573a4b
deepakvats <deepak@mobiloitte.in>
2016-12-13 15:26:59 +0530

    conflicts resolved

--- file_handler/functionHandler.js
+++ file_handler/functionHandler.js
@@ -1,7 +1,7 @@
   var https = require('https');
   var nodemailer = require('nodemailer');
   var FCM = require('fcm-push');
-  var apn = require('apn');
+  //var apn = require('apn');
   module.exports = {
       "otp": function(req, res, mobile) {
           var possible = "0123456789";
@@ -62,67 +62,67 @@
 
       },
 
-       "android_notification": function(deviceToken, message) {
-         var serverKey = 'AIzaSyDGz-6yYBGWStanh-DN7ljMzDATDFGA9vQ';
-         var fcm = new FCM(serverKey);
+   "android_notification": function(deviceToken, message) {
+          var serverKey = 'AIzaSyDGz-6yYBGWStanh-DN7ljMzDATDFGA9vQ';
+          var fcm = new FCM(serverKey);
 
-         var message = {
-             to: deviceToken, // required
-             collapse_key: 'your_collapse_key',
-             data: {
-                 your_custom_data_key: 'your_custom_data_value'
-             },
-             notification: {
-                 title: 'Hello from Brolix.',
-                 body:  message
-             }
-         };
+          var message = {
+              to: deviceToken, // required
+              collapse_key: 'your_collapse_key',
+              data: {
+                  your_custom_data_key: 'your_custom_data_value'
+              },
+              notification: {
+                  title: 'Hello from Brolix.',
+                  body: message
+              }
+          };
 
-         fcm.send(message, function(err, response) {
-             if (err) {
-                 console.log("Android !! Something has gone wrong!");
-             } else {
-                 console.log("Successfully sent with response: ", response);
-             }
-         });
-     },
+          fcm.send(message, function(err, response) {
+              if (err) {
+                  console.log("Android !! Something has gone wrong!");
+              } else {
+                  console.log("Successfully sent with response: ", response);
+              }
+          });
+      },
 
-         "iOS_notification": function(deviceToken, message) {
-         var options = {
-             "cert": "MobiloitteEnterprise.pem",
-             "key": "MobiloitteEnterprise.pem",
-             "passphrase": "Mobiloitte1",
-             //    "cert": "pushcert.pem",
-             //    "key":  "pushcert.pem",
-             //    "passphrase": "",
-             "gateway": "gateway.push.apple.com",
-             "port": 2195,
-             "enhanced": true,
-             "cacheLength": 5,
-             "title": "Testing",
-             "message": "Hello from Koob. We have new titles in our library. Click here to see!",
-             "deviceToken": deviceToken
-         };
-         var title = "Hello from Brolix";
-         var message = message;
-         var bookname = bookname;
-         var deviceToken = deviceToken;
-         var apnConnection = new apn.Connection(options);
-         var myDevice = new apn.Device(deviceToken);
-         var note = new apn.Notification();
-         note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-         note.badge = 1;
-         note.alert = message;
-         note.payload = {
-             "title": title,
-             "message": message
-         };
-         try {
-             apnConnection.pushNotification(note, myDevice);
-         } catch (ex) {
-             console.log(ex);
-         }
-         console.log('iOS Push Notification send');
-     }
+  "iOS_notification": function(deviceToken, message) {
+          var options = {
+              "cert": "MobiloitteEnterprise.pem",
+              "key": "MobiloitteEnterprise.pem",
+              "passphrase": "Mobiloitte1",
+              //    "cert": "pushcert.pem",
+              //    "key":  "pushcert.pem",
+              //    "passphrase": "",
+              "gateway": "gateway.push.apple.com",
+              "port": 2195,
+              "enhanced": true,
+              "cacheLength": 5,
+              "title": "Testing",
+              "message": "Hello from Koob. We have new titles in our library. Click here to see!",
+              "deviceToken": deviceToken
+          };
+          var title = "Hello from Brolix";
+          var message = message;
+          var bookname = bookname;
+          var deviceToken = deviceToken;
+          var apnConnection = new apn.Connection(options);
+          var myDevice = new apn.Device(deviceToken);
+          var note = new apn.Notification();
+          note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+          note.badge = 1;
+          note.alert = message;
+          note.payload = {
+              "title": title,
+              "message": message
+          };
+          try {
+              apnConnection.pushNotification(note, myDevice);
+          } catch (ex) {
+              console.log(ex);
+          }
+          console.log('iOS Push Notification send');
+      }
 
   }
