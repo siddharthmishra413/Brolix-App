e0811b969ede06a357b3d571f740c2491d6f6db0
deepakvats <deepak@mobiloitte.in>
2016-12-14 18:33:59 +0530

    show all user api in admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
-var userHandler = require('../file_handler/user.js');
+var adminHandler = require('../file_handler/admin.js');
 var authUser = require('../middlewares/authUser');
 
 
-// app.post('/changePassword',authUser.authUser,userHandler.changePassword);
+ app.get('/showAllUsershowAllUser',authUser.authUser,adminHandler.showAllUser);
+ app.get('/winners',authUser.authUser,adminHandler.winners);
+ app.post('/sendBrolixToFollower',authUser.authUser,adminHandler.sendBrolixToFollower);
 
 
 module.exports = app;	
