6cd0693d2f79d20367070d129f90828300bb1484
deepakvats <deepak@mobiloitte.in>
2016-12-19 18:55:07 +0530

    show all user api

--- file_handler/admin.js
+++ file_handler/admin.js
@@ -32,13 +32,7 @@ module.exports = {
 
     "sendBrolix": function(req, res) {
         User.findOne({ _id: req.body.userId }, function(err, result) {
-            if (err) { res.send({ responseCode: 500, responseMessage: 'Internal server error' }); }
-
-            // if (result.brolix <= req.body.brolix) { res.send({ responseCode: 400, responseMessage: "Insufficient amount of Brolix in your account" }); } 
-            else {
-                //     result.brolix -= req.body.brolix;
-                //     result.save();
-
+            if (err) { res.send({ responseCode: 500, responseMessage: 'Internal server error' }); } else {
                 User.findOneAndUpdate({ _id: req.body.receiverId }, { $push: { "sendBrolixListObject": { senderId: req.body.userId, brolix: req.body.brolix } } }, { new: true }, function(err, results) {
                     if (err) { res.send({ responseCode: 409, responseMessage: 'Internal server error' }); } else if (!results) res.send({ responseCode: 404, responseMessage: "please enter correct userId" });
                     else {
@@ -52,22 +46,6 @@ module.exports = {
                     }
                 });
             }
-        });
-    },
-
-
-    "blockUser": function(req, res) {
-        console.log("block user exports-->>>" + JSON.stringify(req.body));
-        User.findByIdAndUpdate({ _id: req.body.userId }, { '$set': { 'status': 'BLOCK' } }, { new: true }, function(err, result) {
-            if (err) { res.send({ responseCode: 409, responseMessage: 'Internal server error' }); } else if (!result) return res.status(404).send({ responseMessage: "please enter correct adId" })
-            else {
-                res.send({
-                    // result: result,
-                    responseCode: 200,
-                    responseMessage: "User Blocked successfully!!"
-                });
-            }
-
         });
     }
 
--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,6 @@ var authUser = require('../middlewares/authUser');
 app.get('/showAllUsershowAllUser', authUser.authUser, adminHandler.showAllUser);
 app.get('/winners', authUser.authUser, adminHandler.winners);
 app.post('/sendBrolix', authUser.authUser, adminHandler.sendBrolix);
-app.post('/blockUser', authUser.authUser, adminHandler.blockUser);
 
 
 module.exports = app;
