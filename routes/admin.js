325f73fcd9d08890fb60f44edd467b1702922734
deepakvats <deepak@mobiloitte.in>
2016-12-19 14:49:30 +0530

    before merge

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,10 @@ var adminHandler = require('../file_handler/admin.js');
 var authUser = require('../middlewares/authUser');
 
 
- app.get('/showAllUsershowAllUser',authUser.authUser,adminHandler.showAllUser);
- app.get('/winners',authUser.authUser,adminHandler.winners);
- app.post('/sendBrolixToFollower',authUser.authUser,adminHandler.sendBrolixToFollower);
+app.get('/showAllUsershowAllUser', authUser.authUser, adminHandler.showAllUser);
+app.get('/winners', authUser.authUser, adminHandler.winners);
+app.post('/sendBrolix', authUser.authUser, adminHandler.sendBrolix);
+app.post('/blockUser', authUser.authUser, adminHandler.blockUser);
 
 
-module.exports = app;	
+module.exports = app;
