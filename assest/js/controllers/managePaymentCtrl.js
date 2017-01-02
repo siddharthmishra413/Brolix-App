da9121f17389560d32eb1b60ac0a7d9f3a673e07
Akkiakash <akash.bais@mobiloitte.in>
2017-01-02 19:17:41 +0530

    Change Done 2-Jan-2017

--- assest/js/controllers/managePaymentCtrl.js
+++ assest/js/controllers/managePaymentCtrl.js
@@ -2,6 +2,6 @@ app.controller('managePaymentCtrl', function ($scope,$window) {
 $(window).scrollTop(0,0);
 console.log("managePaymentCtrl");
 $scope.$emit('headerStatus', 'Manage Paymant');
- $scope.$emit('SideMenu', 'Manage Paymant');
+  $scope.$emit('SideMenu', 'Manage Paymant');
 
 })
