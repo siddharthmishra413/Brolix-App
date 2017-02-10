5b8448b546435f2ce74c654dc2d50f4e4eb08b2f
deepakvats <deepak@mobiloitte.in>
2017-02-10 15:46:57 +0530

    Changes one more api in page.js

--- file_handler/page.js
+++ file_handler/page.js
@@ -95,7 +95,7 @@ module.exports = {
     "showPageDetails": function(req, res) {
         var date = new Date().toUTCString()
         console.log(JSON.stringify(date))
-        createNewPage.findOne({ _id: req.body.pageId, status: "ACTIVE" }).populate({ path: 'pageFollowersUser.userId', select: ('firstName lastName image country state city') }).exec(function(err, result) {
+        createNewPage.findOne({ _id: req.body.pageId, status: "ACTIVE" }).populate({ path: 'pageFollowersUser.userId', select: ('firstName lastName image country state city') }).populate({ path: 'adAdmin.userId', select: ('firstName lastName image country state city') }).exec(function(err, result) {
             if (err) { res.send({ responseCode: 409, responseMessage: 'Internal server error' }); }
             createEvents.find({ pageId: req.body.pageId, status: "ACTIVE", createdAt: { $gte: date } }).exec(function(err, result1) {
                 if (err) { res.send({ responseCode: 409, responseMessage: 'Internal server error' }); }
