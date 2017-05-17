758d03d422ac37c2d24f8036f96bb7163297e6b2
mobiloitte_vardan <vardan.agarwal@mobiloittegroup.com>
2017-05-17 05:40:50 +0000

    merging with deepak

--- assest/js/controllers/addUserCtrl.js
+++ assest/js/controllers/addUserCtrl.js
@@ -41,28 +41,29 @@ app.controller('addUserCtrl', function($scope, $state, $window, userService, $ht
     }
 
     $scope.cancel = function() {
-        $scope.myFrom = '';
-        $scope.userFrom.firstName.$dirty = false;
-        $scope.userFrom.firstName.$invalid = false;
-        $scope.userFrom.firstName.$error.required = false;
-        $scope.userFrom.lastName.$dirty = false;
-        $scope.userFrom.lastName.$invalid = false;
-        $scope.userFrom.lastName.$error.required = false;
-        $scope.userFrom.dob.$dirty = false;
-        $scope.userFrom.dob.$invalid = false;
-        $scope.userFrom.dob.$error.required = false;
-        $scope.userFrom.email.$dirty = false;
-        $scope.userFrom.email.$invalid = false;
-        $scope.userFrom.email.$error.required = false;
-        $scope.userFrom.city.$dirty = false;
-        $scope.userFrom.city.$invalid = false;
-        $scope.userFrom.city.$error.required = false;
-        $scope.userFrom.mobileNumber.$dirty = false;
-        $scope.userFrom.mobileNumber.$invalid = false;
-        $scope.userFrom.mobileNumber.$error.required = false;
-        $scope.userFrom.country.$dirty = false;
-        $scope.userFrom.country.$invalid = false;
-        $scope.userFrom.country.$error.required = false;
+        $state.go('header.manageUsers');
+        // $scope.myFrom = '';
+        // $scope.userFrom.firstName.$dirty = false;
+        // $scope.userFrom.firstName.$invalid = false;
+        // $scope.userFrom.firstName.$error.required = false;
+        // $scope.userFrom.lastName.$dirty = false;
+        // $scope.userFrom.lastName.$invalid = false;
+        // $scope.userFrom.lastName.$error.required = false;
+        // $scope.userFrom.dob.$dirty = false;
+        // $scope.userFrom.dob.$invalid = false;
+        // $scope.userFrom.dob.$error.required = false;
+        // $scope.userFrom.email.$dirty = false;
+        // $scope.userFrom.email.$invalid = false;
+        // $scope.userFrom.email.$error.required = false;
+        // $scope.userFrom.city.$dirty = false;
+        // $scope.userFrom.city.$invalid = false;
+        // $scope.userFrom.city.$error.required = false;
+        // $scope.userFrom.mobileNumber.$dirty = false;
+        // $scope.userFrom.mobileNumber.$invalid = false;
+        // $scope.userFrom.mobileNumber.$error.required = false;
+        // $scope.userFrom.country.$dirty = false;
+        // $scope.userFrom.country.$invalid = false;
+        // $scope.userFrom.country.$error.required = false;
     }
 })
 
